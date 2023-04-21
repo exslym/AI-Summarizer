f9c03bc138a74fb99fb1ec4a6816ae3b5c3b4bf4
exslym <76844562+exslym@users.noreply.github.com>
2023-04-21 20:16:41 +0400

    configs updated

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import copy from './copy.svg';
+import linkIcon from './link.svg';
+import loader from './loader.svg';
+import logo from './logo.svg';
+import tick from './tick.svg';
+
+export { copy, linkIcon, loader, logo, tick };
